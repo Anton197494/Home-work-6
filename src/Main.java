c40d26693635218f1ed0f145f1cba7d561eea403
Admin <vishlin.a.k@gmail.com>
2025-05-24 17:06:03 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        task1(1);
+
+
+
+
+
+
+    }
+    public static void task1(int i) {
+        for (; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+
     }
 }
